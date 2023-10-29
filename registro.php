1cfb4fd599ed1819cdb3ce06155e2296cc90a983
sraneb <108536495+sraneb@users.noreply.github.com>
2023-10-29 14:28:37 -0300

    Update registro.php

--- registro.php
+++ registro.php
@@ -5,46 +5,51 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário de Cadastro</title>
+    <link rel="stylesheet" href="1.css">
     <style>
         body {
             font-family: Arial, sans-serif;
-            background-color: #f5f5f5;
+            background-color: #f4f4f4;
+            text-align: center;
+            margin: 0;
+            padding: 0;
         }
-
         form {
-            background: #fff;
+            background-color: #fff;
             max-width: 400px;
             margin: 0 auto;
             padding: 20px;
             border: 1px solid #ccc;
             border-radius: 5px;
-            box-shadow: 0px 0px 10px #ccc;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
-
+        h1 {
+            font-size: 24px;
+        }
         label {
             display: block;
-            margin-bottom: 5px;
+            margin-top: 10px;
         }
-
-        input[type="text"],
-        input[type="email"],
-        input[type="password"] {
+        input {
             width: 100%;
             padding: 10px;
+            margin-top: 5px;
             margin-bottom: 10px;
             border: 1px solid #ccc;
-            border-radius: 5px;
+            border-radius: 3px;
         }
-
+        input[type="radio"] {
+            margin-right: 5px;
+        }
         button {
             background-color: #007bff;
             color: #fff;
-            padding: 10px 20px;
             border: none;
-            border-radius: 5px;
+            padding: 10px 20px;
+            margin-top: 10px;
+            border-radius: 3px;
             cursor: pointer;
         }
-
         button:hover {
             background-color: #0056b3;
         }
@@ -57,16 +62,20 @@
         <label for="nome">Nome:</label>
         <input type="text" id="nome" name="nome" required>
 
-        <label for="email">email:</label>
-        <input type="text" id="email" name="email" required>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
 
         <label for="senha">Senha:</label>
         <input type="password" id="senha" name="senha" required>
 
-        <label for="genero">genero:</label>
-        <input type="text" id="genero" name="genero" required>
-        
+        <label>Gênero:</label>
+        <Br>
+    </Br>
+        <label for="genero-masculino">Masculino</label>
+        <input type="radio" id="genero" name="genero" value="Masculino" required>
 
+        <label for="genero-feminino">Feminino</label>
+        <input type="radio" id="genero" name="genero" value="Feminino" required>
         <button type="submit">Cadastrar</button>
     </form>
 </body>

972ebb5682f1e6ab426b40a131fe952dc3c45329
sraneb <108536495+sraneb@users.noreply.github.com>
2023-11-06 06:17:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,42 @@
 <?php
-include("conexao.php");
-include("cadastro.php");
-if (isset($_POST['email']) == 0){
-  
-  if(strlen($_POST['email']) == 0 ){
-    echo "preencha seu email";
-  } else if(strlen($_POST['senha']) == 0){
-    echo "preencha sua senha";
-  } else {
+include('conexao.php');
 
-      $email = $mysqli-> real_escaoe_string($_POST['email']);
-      $senha = $mysqli-> real_escaoe_string($_POST['seha']);
+if(isset($_POST['email']) || isset($_POST['senha'])) {
 
-      $sql_code = "SELECT *FROM usuarios WHERE email='$email' AND senha ='$senha'";
-      $sql_query = $mysqli->query($sql_code) or die ("Falha na execução do código SQL:". $mysqli->error):
+    if(strlen($_POST['email']) == 0) {
+        echo "Preencha seu e-mail";
+    } else if(strlen($_POST['senha']) == 0) {
+        echo "Preencha sua senha";
+    } else {
 
-      $quantidade = $sql_query->num_rows;
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
 
-      if($quantidade == 1) {
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
 
-      $usuario =$sql_query->fetch_assoc();
+        $quantidade = $sql_query->num_rows;
 
-      if (! isset($_SESSION)){
-          session_start();
-      }    
+        if($quantidade == 1) {
+            
+            $usuario = $sql_query->fetch_assoc();
 
-      $_SESSION['id'] = $usuario['id'];
-      $_SESSION['nome'] = $usuario['nome'];
+            if(!isset($_SESSION)) {
+                session_start();
+            }
 
-      heander("location: inicio.php");
-   
-   } else{
-        echo "falha ao logar! email ou senha incorretos ";  
-   }
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
 
+            header("Location: painel.php");
 
+        } else {
+            echo "Falha ao logar! E-mail ou senha incorretos";
+        }
 
+    }
 
-
-
+}
 ?>
 
 

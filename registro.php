2c58059afa50eb75cfdf1eaf9096f23aaa23683d
sraneb <108536495+sraneb@users.noreply.github.com>
2023-11-16 06:36:50 -0300

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,82 +1,183 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário de Cadastro</title>
-    <link rel="stylesheet" href="1.css">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            text-align: center;
-            margin: 0;
-            padding: 0;
-        }
-        form {
-            background-color: #fff;
-            max-width: 400px;
-            margin: 0 auto;
-            padding: 20px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-        h1 {
-            font-size: 24px;
-        }
-        label {
-            display: block;
-            margin-top: 10px;
-        }
-        input {
-            width: 100%;
-            padding: 10px;
-            margin-top: 5px;
-            margin-bottom: 10px;
-            border: 1px solid #ccc;
-            border-radius: 3px;
-        }
-        input[type="radio"] {
-            margin-right: 5px;
-        }
-        button {
-            background-color: #007bff;
-            color: #fff;
-            border: none;
-            padding: 10px 20px;
-            margin-top: 10px;
-            border-radius: 3px;
-            cursor: pointer;
-        }
-        button:hover {
-            background-color: #0056b3;
-        }
-    </style>
+  <script src="script.js"></script>
+  <link rel="stylesheet" href="login.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>registro</title>
 </head>
+<style>
+
+
+
+body{
+margin: 0;
+}
+body *{
+  box-sizing: border-box;
+}
+.main-login{ width: 100vw;
+height: 100vh;
+background-image:linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7) ), url('deserto.jpg');
+display: flex;
+justify-content: center;
+align-items: center;
+}
+
+.left-login{
+width: 50vw;
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+
+}
+.mbd-image{
+  width: 35vw;
+}
+
+
+
+.Tlt1{
+color: #77ffc0;
+
+}
+
+.right-login{
+ width: 50vw;
+ height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+}
+.card-login{
+ width:60%;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+padding: 30px 35px;
+background-color: #2f2841;
+border-radius: 20px;
+box-shadow: 0px 10px 40px #00000056;
+}
+.card-login h1{
+color: #77ffc0;
+font-weight: 800;
+ margin:0; 
+}
+.textfield{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+}
+  .textfield input{
+    width: 100%;
+    border: none;
+    border-radius: 10px;
+    padding: 15px;
+    background-color: #514869;
+    color: #f0ffffde;
+    font-size: 12pt;
+    box-shadow: 0px 10px 40px #00000056;
+    outline: none;
+    box-sizing: border-box;
+  }
+  .textfield label {
+  color: #f0ffffde;
+  margin-bottom: 10px;
+  }
+  .textfield input::placeholder{
+    color: #f0ffff94;
+  }
+  .botao-login{
+   width: 100%;
+   padding: 16px 0px;
+   margin: 25px;
+   border: none;
+   border-radius: 8px;
+    outline: none;
+    text-transform: uppercase;
+    font-weight: 800;
+    letter-spacing: 1px;
+    color: #2f2841;
+    background-color: #77ffc0;
+    cursor: pointer;
+    box-shadow: 0px 10px 40px -12px #138550;
+  }
+  @Media only screen and (max-width:950px){
+    .card-login{
+      width: 85%;
+    }
+  } 
+  @Media only screen and (max-width:600px){
+    .main-login{
+      flex-direction: column;
+    }
+    .left-login >h1{
+      display: none;
+    }
+    .left-login{
+      width: 100%;
+      height: auto;
+    }
+    .right-login{
+      width: 100%;
+      height: auto;
+    }
+    .left-login-image{
+      width: 50vw;
+    }
+    .card-login{
+      width: 90;
+    }
+  } 
+  
+</style>
 <body>
-    <form action="cadastro.php" method="POST">
-        <h1>Cadastro</h1>
+  <div class="main-login">
+    <div class="left-login"></div>
+    <h1 class="Tlt1">Faça o cadastro<br>Para uma vida mais saudável!</h1>
+    <img src = "slow.png " width="400" height="400" alt = "400" style = "" > 
 
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome" required>
+    <div class="right-login"></div>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
+    <div class="card-login">
+      <h1>Cadastro</h1>
 
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha" required>
+      <form action="cadastro.php" method="POST"> <!-- Adicionado o formulário com a ação para cadastro.php -->
+        <div class="textfield">
+          <label for="usuario">Nome:</label>
+          <input type="text" name="nome" placeholder="Nome">
+        </div>
 
-        <label>Gênero:</label>
-        <Br>
-    </Br>
-        <label for="genero-masculino">Masculino</label>
-        <input type="radio" id="genero" name="genero" value="Masculino" required>
+        <div class="textfield">
+          <label for="usuario">Email:</label>
+          <input type="text" name="email" placeholder="Email">
+        </div>
 
-        <label for="genero-feminino">Feminino</label>
-        <input type="radio" id="genero" name="genero" value="Feminino" required>
-        <button type="submit">Cadastrar</button>
-    </form>
+        <div class="textfield">
+          <label for="senha">Senha:</label>
+          <input type="password" name="senha" placeholder="Digite sua senha aqui...">
+        </div>
+
+        <div class="textfield">
+          <label for="genero">Gênero:</label>
+          <select name="genero">
+            <option value="masculino">Masculino</option>
+            <option value="feminino">Feminino</option>
+          </select>
+        </div>
+    
+        <button type="submit" class="botao-login">Cadastrar</button> <!-- Botão para enviar o formulário -->
+      </form>
+
+        <button type="submit" class="botao-login"><a href="login.php">Página de Login</a></button>
+    </div>
+  </div>
 </body>
 </html>
